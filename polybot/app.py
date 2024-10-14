35d52d5c0f1fb9403588d1388f1ad409d137256c
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-10-14 20:21:09 +0300

    sending a retults message to user with information on image uploaded

--- polybot/app.py
+++ polybot/app.py
@@ -2,14 +2,18 @@ import flask
 from flask import request
 import os
 from bot import ObjectDetectionBot
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 app = flask.Flask(__name__)
 
 
 # TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+TELEGRAM_TOKEN = '7135044116:AAFdJzL1kosGaJduLOl30tGI5j1Whq-1D2c'
 
-TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+# TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+TELEGRAM_APP_URL = 'https://legal-regularly-midge.ngrok-free.app'
 
 
 @app.route('/', methods=['GET'])
@@ -26,13 +30,26 @@ def webhook():
 
 @app.route(f'/results', methods=['POST'])
 def results():
-    prediction_id = request.args.get('predictionId')
+    prediction_id = request.args.get('prediction_id')
 
     # TODO use the prediction_id to retrieve results from DynamoDB and send to the end-user
+    table = dynamodb.Table('BotPhotoProject')
+    response = table.get_item(Key={'prediction_id': prediction_id})
 
-    chat_id = ...
-    text_results = ...
+    chat_id = int(response['Item']['chat_id'])
+    objects = response['Item']['labels']
+    dictrespone = {}
+    for x in range(len(objects)):
+        try:
+            dictrespone.update({objects[x]['class']: dictrespone[objects[x]['class']]+1})
+        except:
+            dictrespone[objects[x]['class']] = 1
+    
+    text_results = "The objects is:\n"
+    for keys, values in dictrespone.items():
+        text_results = f"{text_results}{keys}: {values}\n"
 
+    # text_results = response['Item']['labels'][0]['class']
     bot.send_text(chat_id, text_results)
     return 'Ok'
 
