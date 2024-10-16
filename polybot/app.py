2ffd7e85cb4dbfba485a980e6268f870f8f52672
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-10-16 12:56:29 +0300

    Add fuc to get secert from AWS and change url of bot

--- polybot/app.py
+++ polybot/app.py
@@ -3,18 +3,40 @@ from flask import request
 import os
 from bot import ObjectDetectionBot
 import boto3
+import json
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "BotTelegram/token"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    secret = json.loads(secret)
+    return secret["token_telegram"]
+
+# TODO load TELEGRAM_TOKEN value from Secret Manager
+TELEGRAM_TOKEN = get_secret()
+# TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+TELEGRAM_APP_URL = 'https://polybot2.ygdn.online'
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
+
 app = flask.Flask(__name__)
-
-
-# TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = '7135044116:AAFdJzL1kosGaJduLOl30tGI5j1Whq-1D2c'
-
-# TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
-TELEGRAM_APP_URL = 'https://legal-regularly-midge.ngrok-free.app'
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -63,5 +85,4 @@ def load_test():
 
 if __name__ == "__main__":
     bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
-
     app.run(host='0.0.0.0', port=8443)
