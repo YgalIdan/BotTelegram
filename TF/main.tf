0eba843c6a9b938456102e5b2ff9fd68eb34b96c
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-11-10 13:07:48 +0200

    Fix output ip public to EC2

--- TF/main.tf
+++ TF/main.tf
@@ -212,16 +212,6 @@ data "aws_instances" "BotTelegram_yolov5" {
   depends_on = [aws_autoscaling_group.BotTelegram_autoscaling]
 }
 
-# data "aws_instance" "BotTelegram_instances" {
-#   for_each = toset(data.aws_instances.BotTelegram_yolov5.ids)
-#   instance_id = each.value
-# }
-
 output "ip_ec2_list_botTelegram" {
   value = [for instance in aws_instance.BotTelegram_ec2 : instance.public_ip]
 }
-
-# output "ip_ec2_list_yolov5" {
-#   value = [for instance in data.aws_instance.BotTelegram_instances : instance.public_ip]
-#   # value = data.aws_instances.BotTelegram_yolov5.ids
-# }
