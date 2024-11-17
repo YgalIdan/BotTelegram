725323fc90505259a7e8fcd13e649cbbaaa37a0a
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-11-17 14:39:13 +0200

    Delete all deploy to user_data

--- TF/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt install -y docker.io
-docker run -d --restart always -p 8443:8443 ygalidan/bottelegram:v1.0.3
--- TF/deploy_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt install -y docker.io
-docker run -d --restart always ygalidan/yolo5:v1.0.0
